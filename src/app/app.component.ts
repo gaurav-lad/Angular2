accf1349aec14a98fae5a3a2ac8a11f9d7926f63
gaurav-lad <gaurav.lad02@gmail.com>
2018-03-08 00:11:00 +0530

    nested component, showing table data - slide 6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,12 @@ import {Component} from "@angular/core"
 
 @Component({
     selector: 'my-app',
-    templateUrl: 'app/app.component.html'
+    template: `<div> 
+                    <h1> {{pageHeader}} </h1> 
+                    <my-employee></my-employee>
+              </div>`
 })
+
 export class AppComponent {
-    name: string = "Angular2";
+    pageHeader: string = "Employee Details";
 }

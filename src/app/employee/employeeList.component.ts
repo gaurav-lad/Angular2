aca7576cfac74289aee710e61d7a09eaee4c01c1
gaurav-lad <gaurav.lad02@gmail.com>
2018-03-17 16:12:46 +0530

    Implementing Interface - slide 23

--- src/app/employee/employeeList.component.ts
+++ src/app/employee/employeeList.component.ts
@@ -1,12 +1,12 @@
 ﻿import { Component } from '@angular/core'
-
+import { IEmployee } from './employee';
 @Component({
     selector: 'list-employee',
     templateUrl: 'app/employee/employeeList.component.html',
     styleUrls: ['app/employee/employeeList.component.css']
 })
 export class EmployeeListComponent {
-    employees: any[];
+    employees: IEmployee[];
 
     selectedEmployeeCountRadioButton: string = 'All';
 
